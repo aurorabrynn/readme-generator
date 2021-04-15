0cd88e576b8e474e57033cfe713788295be99dc2
Aurora Duncan <aurorabrynn@gmail.com>
2021-04-14 17:51:18 -0700

    found the generateMarkdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  } else {
+
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  } else {
+    
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  } else {
+    
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+    ## Description
+    ${data.description}
+
+    ## Installation
+    ${data.installation}
+
+    ## Usage
+    ${data.usage}
+
+    ## License
+    ${data.license}
+
+    ## How to Contribute
+    ${data.contribute}
+
+    ## Tests
+    ${data.tests}
+
+    ## Questions?
+    ${data.question}
+    `
 }
 
 module.exports = generateMarkdown;
